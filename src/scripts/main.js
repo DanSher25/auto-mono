2002fb03214912c47c808d45ad082306b83a096b
DanSher25 <dansher250@gmail.com>
2026-03-04 18:23:23 +0300

    slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,7 @@
 import FlipDown from "./vendor/flipDown.js";
 import SlimSelect from "slim-select";
 import Swiper from "swiper";
+import { Autoplay } from "swiper/modules";
 
 import "slim-select/styles";
 import "flipdown/dist/flipdown.css";
@@ -51,14 +52,50 @@ document.addEventListener("DOMContentLoaded", () => {
       input.addEventListener("input", checkForm);
     });
   }
-
   const initHeroSlider = () => {
     const slider = document.querySelector(".hero__list");
     if (!slider) return;
 
     const swiper = new Swiper(slider, {
+      modules: [Autoplay],
+
       slidesPerView: "auto",
       freeMode: true,
+      loop: true,
+      speed: 4000,
+
+      autoplay: {
+        delay: 1,
+        disableOnInteraction: false,
+        pauseOnMouseEnter: true,
+      },
+
+      breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: "auto",
+        },
+      },
+
+      on: {
+        init(swiper) {
+          if (window.innerWidth >= 1024) {
+            swiper.autoplay.stop();
+          }
+        },
+        resize(swiper) {
+          if (window.innerWidth >= 1024) {
+            swiper.autoplay.stop();
+          } else {
+            swiper.autoplay.start();
+          }
+        },
+      },
     });
   };
 
